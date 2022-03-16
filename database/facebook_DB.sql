864afc8956b89d9c8b26e5498364342a07e91dbf
SavouertPheap <savouert.pheap@student.passerellesnumeriques.org>
2022-03-16 10:21:33 +0700

    navbar

--- database/facebook_DB.sql
+++ database/facebook_DB.sql
@@ -1,28 +1,35 @@
 
 
-CREATE TABLE "Users"(
-    "user_id" int primary key auto_increment,
-    "first_name" varchar(25) not null,
-    "last_name" varchar(25) not null,
-    "email" varchar(255) not null,
-    "date_of_birth" date not null,
-    "gender" char(1) not null
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+CREATE TABLE users(
+    user_id int primary key auto_increment,
+    first_name varchar(25) not null,
+    last_name varchar(25) not null,
+    email varchar(255) not null,
+    passwords varchar(50) not null
+    date_of_birth date not null,
+    gender char(1) not null,
 );
 
-CREATE TABLE "Posts"(
-    "post_id" int primary key auto_increment,
-    "title" varchar(255) ,
-    "content_post" varchar(255),
-    "image_name" varchar(255),
-    "user_id" int not null,
-    foreign key ("user_id") references "Users"("user_id")
-);
+CREATE TABLE posts(
+    post_id int primary key auto_increment,
+    title varchar(255) ,
+    content_post varchar(255),
+    image_name varchar(255),
+    user_id int not null,
+    foreign key (user_id) references Users(user_id)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE "Comments"(
-    "comment_id" int primary key auto_increment,
-    "content" varchar(255),
-    "user_id" int not null,
-    "post_id" int not null,
-    foreign key ("user_id") references "Users"("user_id"),
-    foreign key ("post_id") references "Posts" ("post_id")
-)
+CREATE TABLE comments(
+    comment_id int primary key auto_increment,
+    content varchar(255),
+    user_id int not null,
+    post_id int not null,
+    foreign key (user_id) references Users(user_id),
+    foreign key (post_id) references Posts (post_id)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
--- views/post_view.php
+++ views/post_view.php
@@ -1,3 +1,24 @@
+
+<?php
+require_once("templates/header.php")
+ ?>
+ 
 <div class="container">
     <!-- Your code here -->
+    <div class="navigation">
+        <h1>Facebook</h1>
+        <button>Sign In</button>
+        <div class="icon">
+            <ul>
+                <li><i class="fa fa-home"></i></li>
+                <li><i class="fa fa-user-plus"></i></li>
+                <li><i class="fa fa-tv"></i></li>
+                <li><i class="	fa fa-bell"></i></li>
+                <li><i class="fa fa-user-circle"></i></li>
+            </ul>
+            <div class="createpost">
+                <img src="../images/rady.jpg" alt="">
+            </div>
+        </div>
+    </div>
 </div>
