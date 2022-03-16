3606759ffd7c727a7f9cb1d20642eb0f89c5ea69
THIB.TIK <THIB.TIK@PNC.LAN>
2022-03-16 11:17:02 +0700

    post

--- views/post_view.php
+++ views/post_view.php
@@ -1,19 +1,34 @@
-<?php
-require_once('../templates/header.php')
-?>
-
-<div class="container">
-    <!-- Your code here -->
-</div>
-
-
-
-
-
-
-
-
 
 <?php
-require_once('../templates/footer.php.php')
-?>
+require_once("templates/header.php")
+ ?>
+ 
+ <div class="container">
+        <!-- Your code here -->
+        <div class="navigation">
+            <h2>Facebook</h2>
+            <div class="icons">
+                <ul>
+                    <li><i class="fa fa-home fa-2x"></i></li>
+                    <li><i class="fa fa-user-plus fa-2x"></i></li>
+                    <li><i class="fa fa-user-circle fa-2x"></i></li>
+                </ul>
+            </div>
+            <button>Sign In</button>
+        </div>
+        <div class="add-post">
+            <div class="cirles-profile">
+                <div class="profile">
+                    <img src="images/rady.jpg" alt="">
+                </div>
+                <input type="text" name="content" placeholder="What is your mind? ...">
+            </div>
+            <div class="buttons">
+
+                <button type="submit">Add Posts</button>
+            </div>
+        </div>
+    </div>
+<?php
+require_once("templates/footer.php")
+ ?>
